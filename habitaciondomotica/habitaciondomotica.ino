09f749e3f63852e9c2850b51b76837db82d1d7ef
pablonhernandez <pablo.nhernandez@alumnos.upm.es>
2019-04-26 11:09:56 +0200

    Update habitaciondomotica.ino

--- habitaciondomotica/habitaciondomotica.ino
+++ habitaciondomotica/habitaciondomotica.ino
@@ -1,19 +1,24 @@
 
 const int pir= 2;     //Pin es el  2
+const int led= 3;     //LED en pin 3
 int estadopir;          //Detección o no de presencia
 
 void setup() 
 {
   pinMode(pir, INPUT);     //Es un dispositivo de entrada
+  pinMode (led, OUTPUT);   //Es un pin de salida
 }
 
 void loop() 
 {
   int value= digitalRead(pir); //Leer pir
   if (value == HIGH)                  //Si detecta presencia
-  //Va un else pero no me deja ponerlo de momento
-  {
-                                     // Deteccion de presencia activado
+    {
+      digitalWrite(led,HIGH);                         // Deteccion de presencia activado
   }    
-}
+  else {
+      digitalWrite(led,LOW);
+  }
+  }
+
  
