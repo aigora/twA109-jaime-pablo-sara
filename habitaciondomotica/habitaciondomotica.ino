1a823308c83037b31751a753ca2c430416701614
Jaime Palomino Vaquero <jaime.palomino.vaquero@alumnos.upm.es>
2019-04-26 12:41:47 +0200

    Update habitaciondomotica.ino

--- habitaciondomotica/habitaciondomotica.ino
+++ habitaciondomotica/habitaciondomotica.ino
@@ -1,9 +1,31 @@
 #include <TimeLib.h>
+#define PIN_ANALOGICO A0 // La patilla de Arduino a la que conectamos la entrada (LDR)
+#define ESPERA_LECTURAS 1000 // tiempo en milisegundos entre lecturas de la intensidad de la luz
+   
+long cronometro_lecturas=0;
+long tiempo_transcurrido;
+unsigned int luminosidad;
+float coeficiente_porcentaje=100.0/1023.0;
 const int pir= 2;     //Pin es el  2
 const int led= 3;     //LED en pin 3
 int estadopir;        //Detección o no de presencia
 time_t fecha;         // Declaramos la variable del tipo time_t
 
+int luz (void)
+  {
+    int porcentaje;
+    int dia=0;
+    tiempo_transcurrido=millis()-cronometro_lecturas;
+    if(tiempo_transcurrido>ESPERA_LECTURAS)
+    {
+    cronometro_lecturas=millis();
+    luminosidad=analogRead(PIN_ANALOGICO);
+    porcentaje= luminosidad*coeficiente_porcentaje;
+      if (porcentaje>25)
+        dia=1;
+    }
+     return dia;
+   }
 
 void setup() 
 {
@@ -16,14 +38,18 @@ void setup()
 
 void loop() 
 {
+  
+  int luz (void);
   int value= digitalRead(pir);         //Leer pir
-  if (value == HIGH)                  //Si detecta presencia
+  if (value == HIGH && luz==0)                  //Si detecta presencia y es de noche
     {
       digitalWrite(led,HIGH);         // Enciende el led
       delay (20000);                  //Espera 20 segundos para apagar la luz si no detecta movimiento
     }    
   else                                //Si no detecta presencia
       digitalWrite(led,LOW);          //Se apaga el led
+
+  
 }
   
  
