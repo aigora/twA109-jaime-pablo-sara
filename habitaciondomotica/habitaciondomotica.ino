9ae07954e68c5badc02bb1a851d8c24ce27ecb2f
Jaime Palomino Vaquero <jaime.palomino.vaquero@alumnos.upm.es>
2019-04-30 12:47:48 +0200

    Update habitaciondomotica.ino

--- habitaciondomotica/habitaciondomotica.ino
+++ habitaciondomotica/habitaciondomotica.ino
@@ -1,31 +1,15 @@
 #include <TimeLib.h>
-#define PIN_ANALOGICO A0 // La patilla de Arduino a la que conectamos la entrada (LDR)
+#define PIN_ANALOGICO A0 // El pin del LDR es el A0
 #define ESPERA_LECTURAS 1000 // tiempo en milisegundos entre lecturas de la intensidad de la luz
    
-long cronometro_lecturas=0;
-long tiempo_transcurrido;
-unsigned int luminosidad;
+long cronometro_lecturas=0; //Ponemos el cronometro a 0
+long tiempo_transcurrido; //Tiempo que pasa
+unsigned int luminosidad; //Luminosidad
 float coeficiente_porcentaje=100.0/1023.0;
 const int pir= 2;     //Pin es el  2
 const int led= 3;     //LED en pin 3
 int estadopir;        //Detección o no de presencia
 time_t fecha;         // Declaramos la variable del tipo time_t
-
-int luz (void)
-  {
-    int porcentaje;
-    int dia=0;
-    tiempo_transcurrido=millis()-cronometro_lecturas;
-    if(tiempo_transcurrido>ESPERA_LECTURAS)
-    {
-    cronometro_lecturas=millis();
-    luminosidad=analogRead(PIN_ANALOGICO);
-    porcentaje= luminosidad*coeficiente_porcentaje;
-      if (porcentaje>25)
-        dia=1;
-    }
-     return dia;
-   }
 
 void setup() 
 {
@@ -36,20 +20,49 @@ void setup()
   pinMode (led, OUTPUT);   //Es un pin de salida
 }
 
+int luz (void)   //Funcion de la cantidad de luz
+  {
+    int porcentaje;
+    int dia=0;
+    tiempo_transcurrido=millis()-cronometro_lecturas;
+    if(tiempo_transcurrido>ESPERA_LECTURAS)
+    {
+    cronometro_lecturas=millis();
+    luminosidad=analogRead(PIN_ANALOGICO);
+    porcentaje= luminosidad*coeficiente_porcentaje;
+      if (porcentaje>25) //Si el porcentaje es mayor al 25%, se supone que es de día
+        dia=1;  //Es de día
+    }
+     return dia;
+   }
+
+
+int detector_presencia (void)
+  {
+  int presencia=0;
+  int value= digitalRead(pir);         //Leer pir
+    if (value == HIGH )                  //Si detecta presencia
+       presencia=1; 
+    return presencia;    
+  }
+
+
+void bombilla (int dia, int presencia)
+{
+ if (dia==1 && presencia==1)
+ {
+  digitalWrite(led,HIGH);         // Enciende el led
+  delay (20000);                  //Espera 20 segundos para apagar la luz si no detecta movimiento 
+}
+  else 
+  digitalWrite(led,LOW);          //Se apaga el led
+}
+
 void loop() 
 {
-  
   int luz (void);
-  int value= digitalRead(pir);         //Leer pir
-  if (value == HIGH && luz==0)                  //Si detecta presencia y es de noche
-    {
-      digitalWrite(led,HIGH);         // Enciende el led
-      delay (20000);                  //Espera 20 segundos para apagar la luz si no detecta movimiento
-    }    
-  else                                //Si no detecta presencia
-      digitalWrite(led,LOW);          //Se apaga el led
-
-  
+  int detector_presencia (void);
+  int bombilla (luz, pir);  
 }
   
  
